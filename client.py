8a39c49f0eef182d836b817f14611d472c01328c
Noor Fatima <noorfatima.codes@gmail.com>
2026-02-01 15:39:43 +0500

    Improved client UI

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 from tkinter import *
+from tkinter.scrolledtext import ScrolledText
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("localhost", 2007))
@@ -13,7 +14,8 @@ def sendMsg():
 
     if msg:
         s.send(msg.encode())
-        chatBox.insert(END, msg + "\n")
+        chatBox.insert(END, msg + "\n", "me")
+        chatBox.see(END)
 
     if msg.lower() == "exit":
         closeApp()
@@ -25,7 +27,8 @@ def receiveMsg():
             data = s.recv(1024)
             if not data:
                 break
-            chatBox.insert(END, data.decode() + "\n")
+            chatBox.insert(END, data.decode() + "\n","other")
+            chatBox.see(END)
         except:
             break
 
@@ -42,15 +45,50 @@ def closeApp():
 
 root = Tk()
 root.title("Python Chat App")
+root.geometry("520x620")
+root.configure(bg="#0f172a")
 
-chatBox = Text(root, height=25, width=60)
-chatBox.pack()
+chatBox = ScrolledText(
+    root,
+    bg="#020617",
+    fg="#e2e8f0",
+    font=("Consolas", 11),
+    wrap=WORD,
+    padx=10,
+    pady=10
+)
+chatBox.pack(padx=10, pady=10, fill=BOTH, expand=True)
 
-msgInput = Entry(root, width=50)
-msgInput.pack()
 
-sendBtn = Button(root, text="Send", command=sendMsg)
-sendBtn.pack()
+bottom = Frame(root, bg="#0f172a")
+bottom.pack(fill=X, padx=10, pady=5)
+
+msgInput = Entry(
+    bottom,
+    bg="#1e293b",
+    fg="white",
+    insertbackground="white",
+    font=("Consolas", 11)
+)
+msgInput.pack(side=LEFT, fill=X, expand=True, padx=(0,8))
+
+sendBtn = Button(
+    bottom,
+    text="Send",
+    bg="#2563eb",
+    fg="white",
+    activebackground="#1d4ed8",
+    relief=FLAT,
+    command=sendMsg   
+)
+sendBtn.pack(side=RIGHT)
+msgInput.bind("<Return>", lambda e: sendMsg())
+
+chatBox.tag_config("admin", foreground="#fbbf24")
+chatBox.tag_config("me", foreground="#34d399")
+chatBox.tag_config("other", foreground="#93c5fd")
+
+chatBox.insert(END, "Admin: hello\n", "admin")
 
 root.protocol("WM_DELETE_WINDOW", closeApp)
 
